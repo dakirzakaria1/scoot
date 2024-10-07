0a949a0a7f7e5c17858adaad60d25f29981f175a
dakirzakaria <dakir.03.zakaria.02@gmail.com>
2024-10-07 08:09:27 +0100

    Add lazy loading for background images using Intersection Observer API

--- scripts.js
+++ scripts.js
@@ -8,9 +8,30 @@ const navMenu = document.querySelector(".nav-menu");
 const toggleMenu = () => {
   navMenu.classList.toggle("close-nav-menu");
   overlay.classList.toggle("hidden");
-  menuButtonIcon.src = `../images/icon-menu-${navMenu.classList.contains("close-nav-menu") ? "open" : "close"}.svg`;
+  menuButtonIcon.src = `../images/icon-menu-${
+    navMenu.classList.contains("close-nav-menu") ? "open" : "close"
+  }.svg`;
 };
 
 menuButton.addEventListener("click", toggleMenu);
 overlay.addEventListener("click", toggleMenu);
 
+document.addEventListener("DOMContentLoaded", function () {
+  const lazyBgElements = document.querySelectorAll(".lazy-bg");
+  const lazyLoadBg = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const element = entry.target;
+        const bgImage = element.getAttribute("data-bg");
+        element.style.backgroundImage = `url(${bgImage})`;
+        observer.unobserve(element);
+      }
+    });
+  };
+  const observer = new IntersectionObserver(lazyLoadBg, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  });
+  lazyBgElements.forEach((element) => observer.observe(element));
+});
